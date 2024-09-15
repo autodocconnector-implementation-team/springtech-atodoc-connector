a1d08c7396828cb6a7a337aa7374f0739c24b59f
GGeryx <gorcsos.gergely@gmail.com>
2024-09-15 15:08:00 +0200

    Basics of authentication and login feature. + DbContext

---
+++ src/Infrastructure/AutodocConnector.Persistence/Context/DbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutodocConnector.Persistence.Context
+{
+    /// <summary>
+    /// The db context of the identity database. Data seeding is defined here.
+    /// </summary>
+    public class DbContext : IdentityDbContext
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Data seeding, so the identity db has a role already.
+
+            var userRoleId = "714d2baf-6432-4510-b52a-08c1017049e3";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = userRoleId,
+                    ConcurrencyStamp = userRoleId,
+                    Name = "User",
+                    NormalizedName = userRoleId.ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}

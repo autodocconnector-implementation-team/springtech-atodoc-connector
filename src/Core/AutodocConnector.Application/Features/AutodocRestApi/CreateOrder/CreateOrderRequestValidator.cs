020aecc2acee746f7eeac75151171a6fab8126db
ggeryx <gorcsos.gergely@gmail.com>
2024-10-09 20:04:55 +0200

    Some validation for the CreateOrder request.

--- src/Core/AutodocConnector.Application/Features/AutodocRestApi/CreateOrder/CreateOrderRequestValidator.cs
+++ src/Core/AutodocConnector.Application/Features/AutodocRestApi/CreateOrder/CreateOrderRequestValidator.cs
@@ -1,5 +1,23 @@
 ﻿namespace AutodocConnector.Application.Features.AutodocRestApi.CreateOrder;
 public class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
 {
+    public CreateOrderRequestValidator()
+    {
+        RuleFor(x => x).Must((_, request, context) =>
+        {
+            if (request.Items == null || request.Items.Count == 0)
+            {
+                context.AddFailure("You can't place an order that contains no items.");
+                return false;
+            }
+            if (request.FirstName == null || request.LastName == null)
+            {
+                context.AddFailure("The firstname and lastname fields are mandatory.");
+                return false;
+            }
 
+
+            return true;
+        });
+    }
 }

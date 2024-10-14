3c6e8c3a78af9c9f35291eeaecfa52754a58a8ef
Lizzie219 <cs.lizett9@stud.uni-obuda.hu>
2024-10-14 15:07:39 +0200

    get tracking numbers DTOs

---
+++ src/Core/AutodocConnector.Application/Features/AutodocRestApi/GetTrackingNumbers/DTOs/GetTrackingNumbersResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutodocConnector.Domain.Products.Models;
+using System.Runtime.Serialization;
+
+namespace AutodocConnector.Application.Features.AutodocRestApi.GetTrackingNumbers.DTOs;
+
+/// <summary>
+/// Autodoc get tracking numbers response DTO
+/// </summary>
+public record GetTrackingNumbersResponse : AutodocResponse
+{
+    public string? OrderID { get; set; }
+}
+

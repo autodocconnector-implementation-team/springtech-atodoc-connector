246753004b7a1d3214c89bc6d6d49b980904fdfa
Lizzie219 <cs.lizett9@stud.uni-obuda.hu>
2024-10-28 16:32:33 +0100

    Validator + CommandHandler done

---
+++ src/Core/AutodocConnector.Application/Features/AutodocRestApi/CancelOrder/CancelOrderCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutodocConnector.Application.Features.AutodocRestApi.CancelOrder.DTOs;
+
+namespace AutodocConnector.Application.Features.AutodocRestApi.CancelOrder;
+
+public class CancelOrderCommandHandler : IRequestHandler<CancelOrderRequest, CancelOrderResponse>
+{
+    private readonly CancelOrderValidator _validator;
+    private readonly ICancelOrderRepository _repository;
+
+    public CancelOrderCommandHandler(CancelOrderValidator validator, ICancelOrderRepository repository)
+    {
+        _validator = validator;
+        _repository = repository;
+    }
+
+    public async Task<CancelOrderResponse> Handle(CancelOrderRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            _validator.Validate(request);
+            await _repository.DeleteOrderByOrderIdAsync(request.OrderId);
+            return new CancelOrderResponse();
+        }
+        catch (Exception ex)
+        {
+            var response = new CancelOrderResponse();
+            response.SetError(ex.Message);
+            return response;
+        }
+    }
+}

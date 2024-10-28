7cb31c50ed9d1494599f893674cb5da72a6b0a03
Lizzie219 <cs.lizett9@stud.uni-obuda.hu>
2024-10-28 14:19:19 +0100

    Cancel Order DTOs done

---
+++ src/Core/AutodocConnector.Application/Features/AutodocRestApi/CancelOrder/DTOs/CancelOrderRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace AutodocConnector.Application.Features.AutodocRestApi.CancelOrder.DTOs;
+
+/// <summary>
+/// Autodoc cancel order request DTO
+/// </summary>
+public record CancelOrderRequest : IRequest<CancelOrderResponse>
+{
+    /// <summary>
+    /// Supplier order id - not null
+    /// </summary>
+    public string OrderId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Comment - null if not present
+    /// </summary>
+    public string? Comment { get; set; }
+}
